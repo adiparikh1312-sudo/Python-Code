0b87c8345519472e7984d499a1b385961e3208d3
adiparikh1312-sudo <adi.parikh1312@gmail.com>
2026-02-01 18:46:02 +0530

    Second Commit

---
+++ Lesson2/Activity1.py
@@ -0,0 +1,3 @@
+print("Welcome to ☺️")
+print("How are you\n I am good")
+print("Goodbye", "It was nice to meet you", sep= "😎")
---
+++ Lesson2/Activity2.py
@@ -0,0 +1,5 @@
+x = 5
+y = "Tom"
+print(x,"\n",y)
+name2 = input("enter your name: ")
+print("Hello", name2, "\n How are you?")
