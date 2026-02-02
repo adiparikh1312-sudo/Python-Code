1c08f9e358418705846d64afb40cc7287033051b
adiparikh1312-sudo <adi.parikh1312@gmail.com>
2026-02-02 14:20:34 +0530

    Code 1

---
+++ Lesson2/Lesson2Homework.py
@@ -0,0 +1,11 @@
+a = "Krishna: 2013-12-10"
+b = "Sanshray: 2013-05-07"
+c = "Samarth: 2013-01-25"
+d = "Rishaan: 2013-04-27"
+e = "Vanssh: 2013-08-13"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
